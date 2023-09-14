dddc6338a3a361122bc868df02ecee69a46ae19d
ltrujello <ltrujillo@g.hmc.edu>
2023-09-13 19:16:54 -0700

    Adds code to replace amsthm environemnts

--- latex_to_markdown.py
+++ latex_to_markdown.py
@@ -1,6 +1,89 @@
 import re
 import yaml
 
+# label statement
+label_stmt = re.compile(r"\\label{(.*?)}")
+label_stmt2 = re.compile(r"label{(.*?)}")
+
+# amsthm environments
+definition_stmt = re.compile("\\\\begin{definition}([\s\S]*?)\\\\end{definition}")
+proposition_stmt = re.compile("\\\\begin{proposition}([\s\S]*?)\\\\end{proposition}")
+lemma_stmt = re.compile("\\\\begin{lemma}([\s\S]*?)\\\\end{lemma}")
+corollary_stmt = re.compile("\\\\begin{corollary}([\s\S]*?)\\\\end{corollary}")
+theorem_stmt = re.compile("\\\\begin{theorem}([\s\S]*?)\\\\end{theorem}")
+example_stmt = re.compile("\\\\begin{example}([\s\S]*?)\\\\end{example}")
+description_stmt = re.compile("\\\\begin{example}([\s\S]*?)\\\\end{example}")
+proof_stmt = re.compile("\\\\begin{prf}([\s\S]*?)\\\\end{prf}")
+
+
+repl_def = lambda x: repl_asmthm_statement(x, "definition")
+repl_prop = lambda x: repl_asmthm_statement(x, "proposition" )
+repl_lemma = lambda x: repl_asmthm_statement(x, "lemma")
+repl_corollary = lambda x: repl_asmthm_statement(x, "corollary")
+repl_theorem = lambda x: repl_asmthm_statement(x, "theorem")
+repl_example = lambda x: repl_asmthm_statement(x, "example")
+repl_desc = lambda x: repl_asmthm_statement(x, "description")
+repl_proof = lambda x: repl_asmthm_statement(x, "proof")
+
+
+def repl_asmthm_statement(match, class_name):
+    env_contents = match.group(1)
+    env_contents = textwrap.dedent(env_contents)
+    repl = f"\n<span style=\"display:block\" class=\"{class_name}\">{env_contents}</span>"
+    return repl
+
+def repl_itemize_env(match):
+    code = match.group(0)
+
+    items = []
+    ind = 0
+    while True:
+        curr_match = re.search(item_stmt, code[ind:])
+        next_match = re.search(item_stmt, code[ind + curr_match.end():])
+
+        if next_match is not None:
+            items.append(code[ind + curr_match.end():ind + curr_match.end() + next_match.start()])
+            ind = ind + curr_match.end() + next_match.start()
+
+        else:
+            next_match = re.search(end_itemize_env, code[ind + curr_match.end():])
+            if next_match is not None:
+                items.append(code[ind + curr_match.end(): ind + curr_match.end() + next_match.start()])
+            else:
+                print(f"Failed to find \\item or \\end{{itemize}} in {code[ind + curr_match.end():]}")
+            break
+    res = "\n"
+    for item in items:
+        res += f"* {item}\n\n"
+    return res
+
+
+
+def clean_code(code: str, chapter:int, section: int) -> str:
+    # remove comments
+    code = re.sub(tex_comment, "", code)
+
+    new_code = code
+    ind = 0
+    # get rid of labels for now. alg might be chopping off leading backslash after label.
+    new_code = re.sub(label_stmt, "", new_code)
+    new_code = re.sub(label_stmt2, "", new_code)
+
+    # deindent the code
+    new_code = textwrap.dedent(new_code)
+
+    # amsthm env
+    new_code = re.sub(definition_stmt, repl_def, new_code)
+    new_code = re.sub(proposition_stmt, repl_prop, new_code)
+    new_code = re.sub(lemma_stmt, repl_lemma, new_code)
+    new_code = re.sub(corollary_stmt, repl_corollary, new_code)
+    new_code = re.sub(theorem_stmt, repl_theorem, new_code)
+    new_code = re.sub(example_stmt, repl_example, new_code)
+    new_code = re.sub(description_stmt, repl_desc, new_code)
+    new_code = re.sub(proof_stmt, repl_proof, new_code)
+
+    return new_code
+
 def latex_to_markdown():
     with open("tex/cat_theory_long.tex") as f:
         contents = f.read()
